58f35de310273a4ff072092aa63f791f06e57614
Johannes Gocke <johannes_gocke@hotmail.de>
2018-08-16 12:38:50 +0200

    Added MP5SD EquipmentElement, added MP5SD to MapEquipment. Used Visual Studio Tabify on Events.cs.

--- DemoInfo/Events.cs
+++ DemoInfo/Events.cs
@@ -50,13 +50,13 @@ namespace DemoInfo
 	{
 		public Player Player { get; set; }
 
-		public RoundMVPReason Reason  { get; set; }
+		public RoundMVPReason Reason { get; set; }
 	}
 
 	public class RoundStartedEventArgs : EventArgs
 	{
-		public int TimeLimit  { get; set; }
-		public int FragLimit  { get; set; }
+		public int TimeLimit { get; set; }
+		public int FragLimit { get; set; }
 		public string Objective { get; set; }
 	}
 
@@ -125,12 +125,12 @@ namespace DemoInfo
 		public EquipmentElement NadeType { get; internal set; }
 		public Player ThrownBy { get; internal set; }
 
-		internal NadeEventArgs ()
+		internal NadeEventArgs()
 		{
-		
+
 		}
 
-		internal NadeEventArgs (EquipmentElement type)
+		internal NadeEventArgs(EquipmentElement type)
 		{
 			this.NadeType = type;
 		}
@@ -138,23 +138,23 @@ namespace DemoInfo
 
 	public class FireEventArgs : NadeEventArgs
 	{
-		public FireEventArgs () : base(EquipmentElement.Incendiary)
+		public FireEventArgs() : base(EquipmentElement.Incendiary)
 		{
-			
+
 		}
 	}
 	public class SmokeEventArgs : NadeEventArgs
 	{
-		public SmokeEventArgs () : base(EquipmentElement.Smoke)
+		public SmokeEventArgs() : base(EquipmentElement.Smoke)
 		{
-			
+
 		}
 	}
 	public class DecoyEventArgs : NadeEventArgs
 	{
-		public DecoyEventArgs () : base(EquipmentElement.Decoy)
+		public DecoyEventArgs() : base(EquipmentElement.Decoy)
 		{
-			
+
 		}
 	}
 	public class FlashEventArgs : NadeEventArgs
@@ -163,16 +163,16 @@ namespace DemoInfo
 		public Player[] FlashedPlayers { get; internal set; }
 		//
 
-		public FlashEventArgs () : base(EquipmentElement.Flash)
+		public FlashEventArgs() : base(EquipmentElement.Flash)
 		{
 
 		}
 	}
 	public class GrenadeEventArgs : NadeEventArgs
 	{
-		public GrenadeEventArgs () : base(EquipmentElement.HE)
+		public GrenadeEventArgs() : base(EquipmentElement.HE)
 		{
-			
+
 		}
 	}
 
@@ -258,12 +258,12 @@ namespace DemoInfo
 
 	public class PlayerBindEventArgs : EventArgs
 	{
-		public Player Player {get; set; }
+		public Player Player { get; set; }
 	}
 
 	public class PlayerDisconnectEventArgs : EventArgs
 	{
-		public Player Player {get; set; }
+		public Player Player { get; set; }
 	}
 
 	/// <summary>
@@ -300,8 +300,8 @@ namespace DemoInfo
 	public class SayText2EventArgs : EventArgs
 	{
 		/// <summary>
- 		/// The player who sent the message
- 		/// </summary>
+		/// The player who sent the message
+		/// </summary>
 		public Player Sender { get; set; }
 
 		/// <summary>
@@ -358,7 +358,8 @@ namespace DemoInfo
 		internal int EntityID { get; set; }
 
 		public EquipmentElement Weapon { get; set; }
-		public EquipmentClass Class {
+		public EquipmentClass Class
+		{
 			get
 			{
 				return (EquipmentClass)(((int)Weapon / 100) + 1);
@@ -375,30 +376,31 @@ namespace DemoInfo
 
 		public Player Owner { get; set; }
 
-		public int ReserveAmmo {
-			get {
-				return (Owner != null && AmmoType != -1) ? Owner.AmmoLeft [AmmoType] : -1;
+		public int ReserveAmmo
+		{
+			get
+			{
+				return (Owner != null && AmmoType != -1) ? Owner.AmmoLeft[AmmoType] : -1;
 			}
 		}
 
-		internal Equipment ()
+		internal Equipment()
 		{
 			this.Weapon = EquipmentElement.Unknown;
 		}
 
-		internal Equipment (string originalString)
+		internal Equipment(string originalString)
 		{
 			OriginalString = originalString;
 
-			this.Weapon = MapEquipment (originalString);
-
+			this.Weapon = MapEquipment(originalString);
 		}
 
-		internal Equipment (string originalString, string skin)
+		internal Equipment(string originalString, string skin)
 		{
 			OriginalString = originalString;
 
-			this.Weapon = MapEquipment (originalString);
+			this.Weapon = MapEquipment(originalString);
 
 			SkinID = skin;
 		}
@@ -413,161 +415,167 @@ namespace DemoInfo
 				? OriginalString.Substring(WEAPON_PREFIX.Length)
 				: OriginalString;
 
-			if (OriginalString.Contains ("knife") || OriginalString == "bayonet") {
+			if (OriginalString.Contains("knife") || OriginalString == "bayonet")
+			{
 				weapon = EquipmentElement.Knife;
 			}
 
-			if (weapon == EquipmentElement.Unknown) {
-				switch (OriginalString) {
-				case "ak47":
-					weapon = EquipmentElement.AK47;
-					break;
-				case "aug":
-					weapon = EquipmentElement.AUG;
-					break;
-				case "awp":
-					weapon = EquipmentElement.AWP;
-					break;
-				case "bizon":
-					weapon = EquipmentElement.Bizon;
-					break;
-				case "c4":
-					weapon = EquipmentElement.Bomb;
-					break;
-				case "deagle":
-					weapon = EquipmentElement.Deagle;
-					break;
-				case "decoy":
-				case "decoygrenade":
-					weapon = EquipmentElement.Decoy;
-					break;
-				case "elite":
-					weapon = EquipmentElement.DualBarettas;
-					break;
-				case "famas":
-					weapon = EquipmentElement.Famas;
-					break;
-				case "fiveseven":
-					weapon = EquipmentElement.FiveSeven;
-					break;
-				case "flashbang":
-					weapon = EquipmentElement.Flash;
-					break;
-				case "g3sg1":
-					weapon = EquipmentElement.G3SG1;
-					break;
-				case "galil":
-				case "galilar":
-					weapon = EquipmentElement.Gallil;
-					break;
-				case "glock":
-					weapon = EquipmentElement.Glock;
-					break;
-				case "hegrenade":
-					weapon = EquipmentElement.HE;
-					break;
-				case "hkp2000":
-					weapon = EquipmentElement.P2000;
-					break;
-				case "incgrenade":
-				case "incendiarygrenade":
-					weapon = EquipmentElement.Incendiary;
-					break;
-				case "m249":
-					weapon = EquipmentElement.M249;
-					break;
-				case "m4a1":
-					weapon = EquipmentElement.M4A4;
-					break;
-				case "mac10":
-					weapon = EquipmentElement.Mac10;
-					break;
-				case "mag7":
-					weapon = EquipmentElement.Swag7;
-					break;
-				case "molotov":
-				case "molotovgrenade":
-				case "molotov_projectile":
-					weapon = EquipmentElement.Molotov;
-					break;
-				case "mp7":
-					weapon = EquipmentElement.MP7;
-					break;
-				case "mp9":
-					weapon = EquipmentElement.MP9;
-					break;
-				case "negev":
-					weapon = EquipmentElement.Negev;
-					break;
-				case "nova":
-					weapon = EquipmentElement.Nova;
-					break;
-				case "p250":
-					weapon = EquipmentElement.P250;
-					break;
-				case "p90":
-					weapon = EquipmentElement.P90;
-					break;
-				case "sawedoff":
-					weapon = EquipmentElement.SawedOff;
-					break;
-				case "scar20":
-					weapon = EquipmentElement.Scar20;
-					break;
-				case "sg556":
-					weapon = EquipmentElement.SG556;
-					break;
-				case "smokegrenade":
-					weapon = EquipmentElement.Smoke;
-					break;
-				case "ssg08":
-					weapon = EquipmentElement.Scout;
-					break;
-				case "taser":
-					weapon = EquipmentElement.Zeus;
-					break;
-				case "tec9":
-					weapon = EquipmentElement.Tec9;
-					break;
-				case "ump45":
-					weapon = EquipmentElement.UMP;
-					break;
-				case "xm1014":
-					weapon = EquipmentElement.XM1014;
-					break;
-				case "m4a1_silencer":
-				case "m4a1_silencer_off":
-					weapon = EquipmentElement.M4A1;
-					break;
-				case "cz75a":
-					weapon = EquipmentElement.CZ;
-					break;
-				case "usp":
-				case "usp_silencer":
-				case "usp_silencer_off":
-					weapon = EquipmentElement.USP;
-					break;
-				case "world":
-					weapon = EquipmentElement.World;
-					break;
-				case "inferno":
-					weapon = EquipmentElement.Incendiary;
-					break;
-				case "revolver":
-					weapon = EquipmentElement.Revolver;
-					break;
-				case "scar17"://These crash the game when given via give weapon_[mp5navy|...], and cannot be purchased ingame.
-				case "sg550"://yet the server-classes are networked, so I need to resolve them. 
-				case "mp5navy": 
-				case "p228":
-				case "scout":
-				case "sg552":
-				case "tmp":
-					weapon = EquipmentElement.Unknown;
-					break;
-				default:
-					Trace.WriteLine("Unknown weapon. " + OriginalString, "Equipment.MapEquipment()");
-					break;
+			if (weapon == EquipmentElement.Unknown)
+			{
+				switch (OriginalString)
+				{
+					case "ak47":
+						weapon = EquipmentElement.AK47;
+						break;
+					case "aug":
+						weapon = EquipmentElement.AUG;
+						break;
+					case "awp":
+						weapon = EquipmentElement.AWP;
+						break;
+					case "bizon":
+						weapon = EquipmentElement.Bizon;
+						break;
+					case "c4":
+						weapon = EquipmentElement.Bomb;
+						break;
+					case "deagle":
+						weapon = EquipmentElement.Deagle;
+						break;
+					case "decoy":
+					case "decoygrenade":
+						weapon = EquipmentElement.Decoy;
+						break;
+					case "elite":
+						weapon = EquipmentElement.DualBarettas;
+						break;
+					case "famas":
+						weapon = EquipmentElement.Famas;
+						break;
+					case "fiveseven":
+						weapon = EquipmentElement.FiveSeven;
+						break;
+					case "flashbang":
+						weapon = EquipmentElement.Flash;
+						break;
+					case "g3sg1":
+						weapon = EquipmentElement.G3SG1;
+						break;
+					case "galil":
+					case "galilar":
+						weapon = EquipmentElement.Gallil;
+						break;
+					case "glock":
+						weapon = EquipmentElement.Glock;
+						break;
+					case "hegrenade":
+						weapon = EquipmentElement.HE;
+						break;
+					case "hkp2000":
+						weapon = EquipmentElement.P2000;
+						break;
+					case "incgrenade":
+					case "incendiarygrenade":
+						weapon = EquipmentElement.Incendiary;
+						break;
+					case "m249":
+						weapon = EquipmentElement.M249;
+						break;
+					case "m4a1":
+						weapon = EquipmentElement.M4A4;
+						break;
+					case "mac10":
+						weapon = EquipmentElement.Mac10;
+						break;
+					case "mag7":
+						weapon = EquipmentElement.Swag7;
+						break;
+					case "molotov":
+					case "molotovgrenade":
+					case "molotov_projectile":
+						weapon = EquipmentElement.Molotov;
+						break;
+					case "mp7":
+						weapon = EquipmentElement.MP7;
+						break;
+					case "mp9":
+						weapon = EquipmentElement.MP9;
+						break;
+					case "negev":
+						weapon = EquipmentElement.Negev;
+						break;
+					case "nova":
+						weapon = EquipmentElement.Nova;
+						break;
+					case "p250":
+						weapon = EquipmentElement.P250;
+						break;
+					case "p90":
+						weapon = EquipmentElement.P90;
+						break;
+					case "sawedoff":
+						weapon = EquipmentElement.SawedOff;
+						break;
+					case "scar20":
+						weapon = EquipmentElement.Scar20;
+						break;
+					case "sg556":
+						weapon = EquipmentElement.SG556;
+						break;
+					case "smokegrenade":
+						weapon = EquipmentElement.Smoke;
+						break;
+					case "ssg08":
+						weapon = EquipmentElement.Scout;
+						break;
+					case "taser":
+						weapon = EquipmentElement.Zeus;
+						break;
+					case "tec9":
+						weapon = EquipmentElement.Tec9;
+						break;
+					case "ump45":
+						weapon = EquipmentElement.UMP;
+						break;
+					case "xm1014":
+						weapon = EquipmentElement.XM1014;
+						break;
+					case "m4a1_silencer":
+					case "m4a1_silencer_off":
+						weapon = EquipmentElement.M4A1;
+						break;
+					case "cz75a":
+						weapon = EquipmentElement.CZ;
+						break;
+					case "usp":
+					case "usp_silencer":
+					case "usp_silencer_off":
+						weapon = EquipmentElement.USP;
+						break;
+					case "world":
+						weapon = EquipmentElement.World;
+						break;
+					case "inferno":
+						weapon = EquipmentElement.Incendiary;
+						break;
+					case "revolver":
+						weapon = EquipmentElement.Revolver;
+						break;
+					case "mp5sd":
+						weapon = EquipmentElement.MP5SD;
+						break;
+					case "scar17"://These crash the game when given via give weapon_[mp5navy|...], and cannot be purchased ingame.
+					case "sg550"://yet the server-classes are networked, so I need to resolve them. 
+					case "mp5navy":
+					case "p228":
+					case "scout":
+					case "sg552":
+					case "tmp":
+						weapon = EquipmentElement.Unknown;
+						break;
+					default:
+						Trace.WriteLine("Unknown weapon. " + OriginalString, "Equipment.MapEquipment()");
+						break;
 				}
 			}
 
@@ -598,6 +606,7 @@ namespace DemoInfo
 		Mac10 = 104,
 		UMP = 105,
 		P90 = 106,
+		MP5SD = 107,
 
 		//Heavy
 		SawedOff = 201,
@@ -647,7 +656,5 @@ namespace DemoInfo
 		Rifle = 4,
 		Equipment = 5,
 		Grenade = 6,
-
 	}
 }
-
